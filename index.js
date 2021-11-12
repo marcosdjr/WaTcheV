56a8775bc81302477fdf3390a5fea2158cb8873c
Marcos Domingues Junior <marcos_domingues@hotmail.com>
2021-11-11 23:19:44 -0300

    criação da página Register, atualização de sua rota.

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const port = process.env.PORT || 3001;
+//const port = 3001;
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.listen(port, () =>
+  console.log(`Servidor rodando em http://localhost:${port}`)
+);
