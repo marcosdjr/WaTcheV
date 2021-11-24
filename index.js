90ee01cd9372e7aa71a29dbb63828d4ec5e7c6f2
Marcos Domingues Junior <marcos_domingues@hotmail.com>
2021-11-23 23:34:16 -0300

    Configuração para deploy no Heroku

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 //const port = 3001;
 
 const Movie = require("./models/movie");
